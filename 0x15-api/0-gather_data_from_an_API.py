744e0ffc2b18a489567cf73191720bde05811c6e
Andres Lopez <profesor.de.ingles@aol.com>
2021-05-04 03:25:23 +0000

    ADDED | Script to consume RESTAPI to return TODO list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Get information from the JSONplaceholder API"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    user = requests.get("http://jsonplaceholder.typicode.com/users/" +
+                        argv[1]).json()
+    to_dos = requests.get("http://jsonplaceholder.typicode.com/todos?userId=" +
+                         argv[1]).json()
+    done = requests.get("http://jsonplaceholder.typicode.com/todos?userId=" +
+                        argv[1] + "&completed=true").json()
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get("name"), len(done), len(to_dos)))
+    for task in done:
+        print("\t " + task.get("title"))
